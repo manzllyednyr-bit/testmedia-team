4d69f3e416cc2ee281937b4fa00939cd53d2cdf2
manzllyednyr-bit <manzllyednyr@gmail.com>
2026-08-20 17:39:20 +0800

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -1,9 +1,15 @@
 const createScheduleBtn = document.getElementById("createScheduleBtn");
 const createScheduleEmptyBtn = document.getElementById("createScheduleEmptyBtn");
+const cancelScheduleBtn = document.getElementById("cancelScheduleBtn");
+
 const scheduleFormContainer = document.getElementById("scheduleFormContainer");
 
 function showScheduleForm() {
     scheduleFormContainer.style.display = "block";
+}
+
+function hideScheduleForm() {
+    scheduleFormContainer.style.display = "none";
 }
 
 if (createScheduleBtn) {
@@ -12,4 +18,8 @@ if (createScheduleBtn) {
 
 if (createScheduleEmptyBtn) {
     createScheduleEmptyBtn.addEventListener("click", showScheduleForm);
+}
+
+if (cancelScheduleBtn) {
+    cancelScheduleBtn.addEventListener("click", hideScheduleForm);
 }
